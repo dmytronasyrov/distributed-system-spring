44e4f862d15f9f076b95428a244e5c2e74136ff6
Dmytro Nasyrov <dmytro@pharosproduction.com>
2020-02-15 16:37:41 +0200

    config cloud test

---
+++ limits-service/src/main/java/com/pharosproduction/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,17 @@
+package com.pharosproduction.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+  @Autowired
+  private Configuration configuration;
+
+  @GetMapping("/limits")
+  public LimitsConfiguration retrieveLimitsFromConfigurations() {
+    return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+  }
+}
